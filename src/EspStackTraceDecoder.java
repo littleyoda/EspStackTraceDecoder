144a2644d4de83276537f3ea3415c0215c8b001f
Sven Bursch-Osewold <git@bursch.com>
2016-11-18 19:51:17 +0100

    exception causes with description from https://github.com/me-no-dev/EspExceptionDecoder/commit/3913ab3bb94f97ba8d49b5e1d08757c22ce8cb6d

--- src/EspStackTraceDecoder.java
+++ src/EspStackTraceDecoder.java
@@ -10,39 +10,42 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class EspStrackTraceDecoder  {
+public class EspStackTraceDecoder  {
 	File tool;
 	File elf;
 
-	private static String[] exceptions = {"IllegalInstructionCause",
-			"SyscallCause",
-			"InstructionFetchErrorCause",
-			"LoadStoreErrorCause",
-			"Level1InterruptCause",
-			"AllocaCause",
-			"IntegerDivideByZeroCause",
-			"Reserved for Tensilica",
-			"PrivilegedCause",
-			"LoadStoreAlignmentCause",
-			"Reserved for Tensilica",
-			"InstrPIFDataErrorCause",
-			"LoadStorePIFDataErrorCause",
-			"InstrPIFAddrErrorCause",
-			"LoadStorePIFAddrErrorCause",
-			"InstTLBMissCause",
-			"InstTLBMultiHitCause",
-			"InstFetchPrivilegeCause",
-			"Reserved for Tensilica",
-			"InstFetchProhibitedCause",
-			"Reserved",
-			"Reserved",
-			"Reserved",
-			"LoadStoreTLBMissCause",
-			"LoadStoreTLBMultiHitCause",
-			"LoadStorePrivilegeCause",
-			"Reserved for Tensilica",
-			"LoadProhibitedCause",
-	"StoreProhibitedCause"};
+	private static String[] exceptions = {
+		    "Illegal instruction",
+		    "SYSCALL instruction",
+		    "InstructionFetchError: Processor internal physical address or data error during instruction fetch",
+		    "LoadStoreError: Processor internal physical address or data error during load or store",
+		    "Level1Interrupt: Level-1 interrupt as indicated by set level-1 bits in the INTERRUPT register",
+		    "Alloca: MOVSP instruction, if caller's registers are not in the register file",
+		    "IntegerDivideByZero: QUOS, QUOU, REMS, or REMU divisor operand is zero",
+		    "reserved",
+		    "Privileged: Attempt to execute a privileged operation when CRING ? 0",
+		    "LoadStoreAlignmentCause: Load or store to an unaligned address",
+		    "reserved",
+		    "reserved",
+		    "InstrPIFDataError: PIF data error during instruction fetch",
+		    "LoadStorePIFDataError: Synchronous PIF data error during LoadStore access",
+		    "InstrPIFAddrError: PIF address error during instruction fetch",
+		    "LoadStorePIFAddrError: Synchronous PIF address error during LoadStore access",
+		    "InstTLBMiss: Error during Instruction TLB refill",
+		    "InstTLBMultiHit: Multiple instruction TLB entries matched",
+		    "InstFetchPrivilege: An instruction fetch referenced a virtual address at a ring level less than CRING",
+		    "reserved",
+		    "InstFetchProhibited: An instruction fetch referenced a page mapped with an attribute that does not permit instruction fetch",
+		    "reserved",
+		    "reserved",
+		    "reserved",
+		    "LoadStoreTLBMiss: Error during TLB refill for a load or store",
+		    "LoadStoreTLBMultiHit: Multiple TLB entries matched for a load or store",
+		    "LoadStorePrivilege: A load or store referenced a virtual address at a ring level less than CRING",
+		    "reserved",
+		    "LoadProhibited: A load referenced a page mapped with an attribute that does not permit loads",
+		    "StoreProhibited: A store referenced a page mapped with an attribute that does not permit stores"
+		};
 
 
 	private String[] analyseStracktrace(String content){
@@ -86,7 +89,7 @@ public class EspStrackTraceDecoder  {
 			usage();
 			System.exit(1);
 		}
-		EspStrackTraceDecoder esp = new EspStrackTraceDecoder();
+		EspStackTraceDecoder esp = new EspStackTraceDecoder();
 		esp.elf = new File(args[1]);
 		esp.tool = new File(args[0]);
 		String content = "";
